debfdbaa464eb4bcec9bb745ed97b6b83bd40d6e
Blez_Ten <83898491+Rookie-GIET@users.noreply.github.com>
2022-09-09 17:16:40 +0530

    UI Change

--- app/src/main/java/com/blez/aniplex_clone/Presentation/recentanimerelease/RecentReleaseFragment.kt
+++ app/src/main/java/com/blez/aniplex_clone/Presentation/recentanimerelease/RecentReleaseFragment.kt
@@ -1,6 +1,5 @@
 package com.blez.aniplex_clone.Presentation.recentanimerelease
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,32 +13,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.blez.aniplex_clone.Adapter.RecentAnimeAdapter
 import com.blez.aniplex_clone.Presentation.common.VideoActivity
 import com.blez.aniplex_clone.R
-import com.blez.aniplex_clone.data.ReleaseAnimes
 import com.blez.aniplex_clone.databinding.FragmentRecentAnimeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecentReleaseFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class RecentReleaseFragment : Fragment() {
     private lateinit var binding: FragmentRecentAnimeBinding
     private lateinit var adapter: RecentAnimeAdapter
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -91,23 +77,4 @@ class RecentReleaseFragment : Fragment() {
     }
 }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecentReleaseFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecentReleaseFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
