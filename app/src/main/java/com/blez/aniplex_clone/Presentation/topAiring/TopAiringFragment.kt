2b8fc06c1e694227ca084dbaddc0ee01a5db1f25
Blez_Ten <83898491+Rookie-GIET@users.noreply.github.com>
2022-09-18 21:02:54 +0530

    Added a progress bar

--- app/src/main/java/com/blez/aniplex_clone/Presentation/topAiring/TopAiringFragment.kt
+++ app/src/main/java/com/blez/aniplex_clone/Presentation/topAiring/TopAiringFragment.kt
@@ -18,11 +18,6 @@ import com.blez.aniplex_clone.data.TopAiringData
 import com.blez.aniplex_clone.network.RetrofitInstance
 import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [TopAiringFragment.newInstance] factory method to
@@ -36,10 +31,7 @@ class TopAiringFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -75,23 +67,5 @@ class TopAiringFragment : Fragment() {
         })
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TopAiringFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TopAiringFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
